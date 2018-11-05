1a189f00878166ed4cfd2ec0d06864c9723d44ba
Robert Munteanu <robert@lmn.ro>
2018-11-05 16:05:32 +0100

    Added simple launcher script that rebuilds everything

---
+++ launcher/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+launcher_ver=0.2.0
+launcher_jar=${HOME}/.m2/repository/org/apache/sling/org.apache.sling.feature.launcher/${launcher_ver}/org.apache.sling.feature.launcher-${launcher_ver}.jar
+
+# build
+echo "--- BUILDING PROJECT ---"
+mvn -f .. clean install
+
+# resolve launcher
+if [ ! -f ${launcher_jar} ]; then
+    echo "--- DOWNLOADING LAUNCHER ---"
+    mvn dependency:get -Dartifact=org.apache.sling:org.apache.sling.feature.launcher:${launcher_ver}
+fi
+
+# run
+echo "--- LAUNCHING APPLICATION ---"
+rm -rf launcher target/*  && java -jar ${launcher_jar} -f app.json,config-w541.json
